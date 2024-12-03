6e3353c930aae72947a03a8ea45841f383c192ef
MinaShehata123 <minashehata214@gmail.com>
2024-12-03 17:41:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,7 @@ let menudiv = document.getElementById("menutrue");
 
 document.getElementById("btn").addEventListener('click', function(){
   
-  window.location.href='https://download1085.mediafire.com/1h88ijj1gqlgJNgGhqe3AmhJ6rfbauC3ORXQlCq5k2jnishq3JaN0Bzo5oWrWDlCjikfuhEjcPR_TnNa_DyldLe_t3dPBn-U1j6HyYVf6OOLoQiRbKQNrV9Zw50FZSJ1COyeI-TjPBq80vvV387qvaZMJj-o47xcgegKgkV3tJs7/vimj04ie5fnae8f/%D9%83%D9%86%D9%8A%D8%B3%D8%A9+%D9%85%D8%A7%D8%B1%D8%AC%D8%B1%D8%AC%D8%B3+%D8%A8%D9%85%D8%B7%D8%A7%D9%8A_1.0.apk'
-  
+  window.location.href='https://www.dropbox.com/scl/fi/tzru7uj1bixhenca1rlxh/_1.0.apk?rlkey=kzquksl9xux3khgbicobhc1cq&st=blahzye9&dl=1'
 })
 
 
