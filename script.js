50f763f4c650c3e1ebd24a9d7b8d81f1f0a66c48
MinaShehata123 <minashehata214@gmail.com>
2024-12-05 14:45:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,31 +2,80 @@ let menu = document.getElementById("menuicon");
 
 let menudiv = document.getElementById("menutrue");
 
-document.getElementById("btn").addEventListener('click', function(){
+let facebook = document.getElementById('facebook');
+
+
+facebook.addEventListener('click', function(){
   
-  alert('التطبيق تحت التطوير سيتوفر قريبا ');
+  window.location.href='https://www.facebook.com/groups/stgeorgematay/?ref=share&mibextid=NSMWBT'
+  
+  
 })
 
 
-menu.addEventListener("click" , function() {
+
+let yot = document.getElementById('yot');
+
+
+
+
+
+yot.addEventListener('click', function(){
   
-navigator.vibrate("100");
-
-
-
-
-if (menudiv.style.display === "none" || menudiv.style.display === "") {
-                menudiv.style.display = "block";
-                
-                document.body.style.overflow = 'hidden';
-                
-            } else {
-                menudiv.style.display = "none";
-                
-                document.body.style.overflow = 'auto';
-            }
-    
+  window.location.href='https://youtube.com/@fathertemothawesfarag?si=MsTBgx0HSNuSqsTd'
   
+})
+
+
+
+document.getElementById("btn").addEventListener('click', function() {
+
+    alert('التطبيق تحت التطوير سيتوفر قريبا ');
+})
+
+
+menu.addEventListener("click", function() {
+
+    navigator.vibrate("100");
+
+
+
+
+    if (menudiv.style.display === "none" || menudiv.style.display === "") {
+        menudiv.style.display = "block";
+
+        document.body.style.overflow = 'hidden';
+
+    } else {
+        menudiv.style.display = "none";
+
+        document.body.style.overflow = 'auto';
+    }
+
+
 });
 
 
+
+function showweb() {
+  // Tab to edit
+  
+  
+  document.getElementById("about").style.display="block";
+  
+  
+  document.getElementById("menutrue").style.display="none";
+  
+
+
+}
+
+
+document.getElementById("exit").addEventListener('click', function(){
+  
+  document.getElementById("about").style.display="none";
+  
+  
+  
+  document.body.style.overflow = 'auto';
+})
